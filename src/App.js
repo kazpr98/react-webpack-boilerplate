c5b7b4d95e01053c7e57f014a9ab1663d50f59e5
Luis Garcia <luis.garcia@IE4-CP-MAC081.local>
2020-12-05 13:33:37 +0000

    Adding hotreload to modify without resetting state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {hot} from 'react-hot-loader';
 
 function App() {
+    const [count, setCount] = useState(0);
+
     return (
         <div>
             <h1>Hello world!!</h1>
+            <h2 className={count > 5 ? 'warning' : null}>
+                Count {count}
+            </h2>
+            <button onClick={() => setCount(count => count + 1)}>+</button>
+            <button onClick={() => setCount(count => count - 1)}>-</button>
         </div>
     )
 }
 
-export default App
+export default hot(module)(App)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,8 +14,17 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins: [
+                        '@babel/plugin-proposal-class-properties',
+                        'react-hot-loader/babel'
+                    ]
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader'],
+                exclude: /node_modules/
             }
         ]
     },
