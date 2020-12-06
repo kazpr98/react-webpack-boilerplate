ad537591ebecb426f3dfaab9b5fda89b2be7e004
Luis Garcia <luis.garcia@IE4-CP-MAC081.local>
2020-12-06 13:59:14 +0000

    Adding husky and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    commonjs: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: '17.0.1',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y'],
+};
