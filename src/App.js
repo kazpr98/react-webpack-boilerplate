d3d04a03a98ccf7acca48622bc4d25b108849e6b
Luis Garcia <luis.garcia@IE4-CP-MAC081.local>
2020-12-06 13:01:22 +0000

    Adding jest to the application

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, {useState} from 'react';
-import {hot} from 'react-hot-loader';
+import React, { useState } from "react";
+import { hot } from "react-hot-loader";
+
+const Warning = React.lazy(() => import("./Warning"));
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            <h1>Hello world!!</h1>
-            <h2 className={count > 5 ? 'warning' : null}>
-                Count {count}
-            </h2>
-            <button onClick={() => setCount(count => count + 1)}>+</button>
-            <button onClick={() => setCount(count => count - 1)}>-</button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello world!!</h1>
+      <h2 className={count > 5 ? "warning" : null}>Count {count}</h2>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+      {count > 5 ? (
+        <React.Suspense fallback={null}>
+          <Warning />
+        </React.Suspense>
+      ) : null}
+    </div>
+  );
 }
 
-export default hot(module)(App)
+export default hot(module)(App);
---
+++ src/Warning.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Warning() {
+    return (
+        <span className={'warning'}>
+            Take it ease!
+        </span>
+    )
+}
